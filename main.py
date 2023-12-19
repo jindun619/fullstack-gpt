5c5dee01958779424e1d7ef8231b28c2b13f76b3
jindun619 <jindun619@naver.com>
2023-12-19 22:57:50 +0000

    1

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, AIMessage, SystemMessage
 from langchain.prompts import PromptTemplate, ChatPromptTemplate
-
+from langchain.schema import BaseOutputParser
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 chat = ChatOpenAI(openai_api_key=api_key, temperature=0.1)
 
-template = ChatPromptTemplate.from_messages([
-    ("system", "You are a geography expert. And you only reply in {language}."),
-    ("ai", "Ciao, mi chiamo {name}"),
-    ("human", "What is the distance between {country_a} and {country_b}. Also, what is your name?")
+chef_template = ChatPromptTemplate.from_messages([
+    ("system", "You are a world-class international chef. You create easy to follow recipies for any type of cuisine with easy to find ingredients"),
+    ("human", "I want to cook {} food")
 ])
-prompt = template.format_messages(language="korean", name="Nicolas", country_a="argentina", country_b="brazil")
-
-print(chat.predict_messages(prompt))
