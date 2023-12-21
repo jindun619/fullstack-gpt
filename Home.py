411abeeb29a3066e148682c3fcb71961d6f52d8a
jindun619 <jindun619@naver.com>
2023-12-21 22:54:44 +0000

    created webpage using streamlit

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Hello World!")
+st.subheader("Welcome to Streamlit")
+st.markdown(
+    """
+ #### I love it!
+"""
+)
--- main.py
+++ main.py
@@ -3,16 +3,10 @@ from dotenv import load_dotenv
 
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import get_openai_callback
-
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-
 from langchain.callbacks import StreamingStdOutCallbackHandler
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
-
 llm = ChatOpenAI(
     openai_api_key=api_key,
     temperature=0.1,
@@ -20,25 +14,11 @@ llm = ChatOpenAI(
     callbacks=[StreamingStdOutCallbackHandler()],
 )
 
-memory = ConversationSummaryBufferMemory(
-    llm=llm, max_token_limit=120, memory_key="chat_history"
-)
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
 
-template = """
-    You are a helpful AI talking to a human.
+loader = UnstructuredFileLoader("./files/chapter_one.txt")
+splitter = CharacterTextSplitter.from_tiktoken_encoder(separator="\n", chunk_size=100, chunk_overlap=20)
 
-    {chat_history}
-    Human:{question}
-    You:
-"""
-
-chain = LLMChain(
-    llm=llm,
-    memory=memory,
-    prompt=PromptTemplate.from_template(template),
-    verbose=True,
-)
-
-print(chain.predict(question="Hi, my name is Hoojun"))
-print(chain.predict(question="I live in Seoul"))
-print(chain.predict(question="what is my name?"))
+docs = loader.load()
+print(splitter.split_documents(docs))
