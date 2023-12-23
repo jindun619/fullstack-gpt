98b5969b216b95090cf765d77dc7dc246c67fd17
jindun619 <jindun619@naver.com>
2023-12-23 01:31:00 +0000

    1

--- Home.py
+++ Home.py
@@ -7,3 +7,16 @@ st.markdown(
  #### I love it!
 """
 )
+
+with st.sidebar:
+    st.title("Sidebar Title")
+    st.text_input("input1")
+
+tab1, tab2, tab3 = st.tabs(["a", "b", "c"])
+
+with tab1:
+    st.write("1")
+with tab2:
+    st.write("2")
+with tab3:
+    st.write("3")
