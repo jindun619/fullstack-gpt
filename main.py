e75f95e8935d690d2870e4f14cd8cc05053903ac
jindun619 <jindun619@naver.com>
2024-01-03 22:55:00 +0000

    QuizGPT

--- main.py
+++ main.py
@@ -15,43 +15,13 @@ model = ChatOpenAI(
 )
 
 # -------------------------------------------------------------------------------
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.storage import LocalFileStore
-from langchain.prompts import ChatPromptTemplate
+from langchain.llms import gpt4all
+from langchain.prompts import PromptTemplate
 
-cache_dir = LocalFileStore("./.cache/")
+prompt = PromptTemplate.from_template("A {word} is a")
 
+llm = gpt4all(path="./falcon.bin")
 
-splitter = CharacterTextSplitter.from_tiktoken_encoder(
-    separator="\n",
-    chunk_size=600,
-    chunk_overlap=100,
-)
-loader = UnstructuredFileLoader("./files/chapter_one.txt")
+chain = prompt | llm
 
-docs = loader.load_and_split(text_splitter=splitter)
-
-embeddings = OpenAIEmbeddings()
-
-cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
-
-vectorstore = Chroma.from_documents(docs, cached_embeddings)
-
-retriever = vectorstore.as_retriever()
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful assistant. Answer questions using only the following context. If you don't know the answer just say you don't know, don't make it up:\n\n{context}",
-        ),
-        ("human", "{question}"),
-    ]
-)
-
-chain = {"context": retriever} | prompt | model
-
-chain.invoke({})
+print(chain.invoke({"word": "tomato"}))
--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,3 +1,56 @@
 import streamlit as st
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.retrievers import WikipediaRetriever
 
 st.title("QuizGPT")
+
+
+@st.cache_resource(show_spinner="Loading file..")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+with st.sidebar:
+    docs = None
+    choice = st.selectbox("Choose what you want to use.", ("File", "Wikepedia Article"))
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx, .txt, or .pdf file", type=["docx", "txt", "pdf"]
+        )
+        if file:
+            docs = split_file(file)
+            st.write(docs)
+    else:
+        topic = st.text_input("Search Wikipedia..")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=5)
+            with st.status("Searching Wikipedia.."):
+                docs = retriever.get_relevant_documents(topic)
+                st.write(docs)
+
+if not docs:
+    st.markdown(
+        """
+        Welcome to QuizGPT.
+    
+        I will make a quiz from Wikipedia articles or files you uploaded to test
+        your knowledge and help you study.
+
+        Get started by uploading a file or searching on Wikipedia in the sidebar.
+        """
+    )
+else:
+    st.write(docs)
